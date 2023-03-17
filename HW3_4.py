b54a31143764f318d2241dc0561d15335719355d
Workstation <chistokhin_dmitry@mail.ru>
2023-03-17 23:19:06 +0300

    home work 3

--- HW3_4.py
+++ HW3_4.py
@@ -1,10 +1,8 @@
-n = list(map(int, input("Введите список целых чисел через пробел и нажмите Ентер (чем больше, тем лучше): ").split()))
+a = list(map(int, input("Введите список целых чисел через пробел и нажмите Ентер (чем больше, тем лучше): ").split()))
 
-def change():
-    x = 0
-    for i in range(1, len(n)-1):
-        if n[i] > n[i-1] and n[i] > n[i+1]:
-            x = x + 1
-        return x
+sum=0
+for i in range(1, len(a)-1):
+    if a[i-1] < a[i] > a[i+1]:
+        sum += 1
 
-print(x)
+print("Сумма числе, которые боль ше своих соседей", sum)
