c8f2ac4f0d448f6fb3dc255805e073797d141655
Workstation <chistokhin_dmitry@mail.ru>
2023-03-17 20:25:26 +0300

    home work 3

--- HW3_3.py
+++ HW3_3.py
@@ -1,1 +1,15 @@
-def
+lst = list(map(str, input("Введите список элементов и нажмите Ентер (чем больше, тем лучше): ").split()))
+def change(lst):
+    n = len(lst)
+    x = lst[0]
+    lst[0] = lst[n-1]
+    lst[n-1] = x
+    return lst
+
+print(change(lst))
+
+
+
+
+
+
